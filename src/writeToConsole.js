dd0687b1790af7eab3cd015746ce916750411cc1
med-ridha <ridha.zemzem24@gmail.com>
2022-02-06 15:07:51 +0100

    nice

--- src/writeToConsole.js
+++ src/writeToConsole.js
@@ -7,41 +7,41 @@ module.exports.writeToConsole = function (msg, status, username){
   if(username.length < length){
     space = Array(Math.abs((length+1) - username.length)).join(" ");
   }
-  let columnsLeft = process.stdout.columns - (length+1);
-  if (columnsLeft < 0){
+  let width = process.stdout.columns - (length+1);
+  if (width < 0){
     console.error(`please resize the your terminal atleast ${length} width`);
     process.exit(0);
   }
-  
+
   if(msg.toLowerCase().indexOf('zarga') > -1) tagged = true;
   while (msg.length  !== 0 || username.length !== 0) {
     if(status.indexOf("mod") > -1){
-      //console.log(`${space}${chalk.green(username.substr(0, length)+`|`)}${msg.substr(0, columnsLeft)}`);
+      //console.log(`${space}${chalk.green(username.substr(0, length)+`|`)}${msg.substr(0, width)}`);
       process.stdout.write(`${space}${chalk.green(username.substr(0, length)+`|`)}`)
-      if(tagged)process.stdout.write(`${chalk.red(msg.substr(0, columnsLeft))}`)
+      if(tagged)process.stdout.write(`${chalk.red(msg.substr(0, width))}`)
       else 
-        process.stdout.write(`${msg.substr(0, columnsLeft)}`)
+        process.stdout.write(`${msg.substr(0, width)}`)
     }else if(status.indexOf("vip") > -1){
-      //console.log(`${space}${chalk.magentaBright(username.substr(0, length)+`|`)}${msg.substr(0, columnsLeft)}`);
+      //console.log(`${space}${chalk.magentaBright(username.substr(0, length)+`|`)}${msg.substr(0, width)}`);
       process.stdout.write(`${space}${chalk.magentaBright(username.substr(0, length)+`|`)}`)
-      if(tagged)process.stdout.write(`${chalk.red(msg.substr(0, columnsLeft))}`)
+      if(tagged)process.stdout.write(`${chalk.red(msg.substr(0, width))}`)
       else 
-        process.stdout.write(`${msg.substr(0, columnsLeft)}`)
+        process.stdout.write(`${msg.substr(0, width)}`)
     }else if(status.indexOf("sub") > -1){
-      //console.log(`${space}${chalk.yellowBright(username.substr(0, length)+`|`)}${msg.substr(0, columnsLeft)}`);
+      //console.log(`${space}${chalk.yellowBright(username.substr(0, length)+`|`)}${msg.substr(0, width)}`);
       process.stdout.write(`${space}${chalk.yellowBright(username.substr(0, length)+`|`)}`)
-      if(tagged)process.stdout.write(`${chalk.red(msg.substr(0, columnsLeft))}`)
+      if(tagged)process.stdout.write(`${chalk.red(msg.substr(0, width))}`)
       else 
-        process.stdout.write(`${msg.substr(0, columnsLeft)}`)
+        process.stdout.write(`${msg.substr(0, width)}`)
     }else {
-      //console.log(`${space}${chalk.blue(username.substr(0, length)+`|`)}${msg.substr(0, columnsLeft)}`);
+      //console.log(`${space}${chalk.blue(username.substr(0, length)+`|`)}${msg.substr(0, width)}`);
       process.stdout.write(`${space}${chalk.blue(username.substr(0, length)+`|`)}`)
-      if(tagged)process.stdout.write(`${chalk.red(msg.substr(0, columnsLeft))}`)
+      if(tagged)process.stdout.write(`${chalk.red(msg.substr(0, width))}`)
       else 
-        process.stdout.write(`${msg.substr(0, columnsLeft)}`)
+        process.stdout.write(`${msg.substr(0, width)}`)
     } 
     console.log();
-    msg = msg.substring(columnsLeft, msg.length);
+    msg = msg.substring(width, msg.length);
     username =username.substring(length, username.length);
     space = Array(Math.abs((length+1) - username.length)).join(" ");
   }
